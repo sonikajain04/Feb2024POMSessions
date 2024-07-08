3ef056981000231a81a7314865e413258c590763
sonikajain04 <ssonicajain@gmail.com>
2024-07-08 07:44:52 +0530

    updated the code as per comments

--- src/test/java/com/qa/opencart/tests/DemoPage.java
+++ src/test/java/com/qa/opencart/tests/DemoPage.java
@@ -4,10 +4,16 @@ import org.openqa.selenium.By;
 
 public class DemoPage {
 	By demo = By.id("demo");
+	By cart = By.id("cart");
 	
-	public void getdemo() {
+	public void getDemo() {
 		System.out.println("demo");
+		System.out.println("demo details");
 	}
 	
+	public void getCart() {
+		System.out.println("cart");
+		System.out.println("cart details");
+	}
 
 }

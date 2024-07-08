dbb6509b9828d11c98cde62517e9f675db0f6928
sonikajain04 <ssonicajain@gmail.com>
2024-07-08 07:08:24 +0530

    added Demo Page

---
+++ src/test/java/com/qa/opencart/tests/DemoPage.java
@@ -0,0 +1,13 @@
+package com.qa.opencart.tests;
+
+import org.openqa.selenium.By;
+
+public class DemoPage {
+	By demo = By.id("demo");
+	
+	public void getdemo() {
+		System.out.println("demo");
+	}
+	
+
+}

5f862119dffe9eb79138bf277d578b15c6deb2ea
sonikajain04 <ssonicajain@gmail.com>
2024-07-29 20:30:31 +0530

    ngrok server setup changes

--- src/test/java/com/qa/opencart/tests/DemoPage.java
+++ src/test/java/com/qa/opencart/tests/DemoPage.java
@@ -9,6 +9,7 @@ public class DemoPage {
 	public void getDemo() {
 		System.out.println("demo");
 		System.out.println("demo details");
+		System.out.println("demo ngrok server setup updated 29072024");
 	}
 	
 	public void getCart() {
--- src/test/java/com/qa/opencart/tests/feature.java
+++ src/test/java/com/qa/opencart/tests/feature.java
@@ -3,5 +3,6 @@ package com.qa.opencart.tests;
 public class feature {
 
 	String i = "feature";
+	String j = "updated ngrok feature";
 }
 
